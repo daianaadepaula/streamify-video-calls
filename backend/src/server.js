975960150a92c4d62a32997e4b1fd771bb89376c
daianaadepaula <daianaadepaula1@gmail.com>
2025-05-07 17:17:28 -0300

    feat(backend): onboarding endpoint

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 import express from "express"
 import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
 
 import authRoutes from "./routes/auth.route.js"
 import { connectDB } from "./lib/db.js"
@@ -10,9 +11,10 @@ const app = express()
 const PORT = process.env.PORT || 5001
 
 app.use(express.json())
+app.use(cookieParser())
 app.use("/api/auth", authRoutes)
 
 app.listen(PORT, () => {
-	console.log(`Server is running on port ${PORT}`)
+	console.log(`✅ Server is running on port ${PORT}`)
 	connectDB()
 })
