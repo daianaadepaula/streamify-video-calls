7ad4e59f53eb7e3c7baaf29a976d2f3d7af47ae6
daianaadepaula <daianaadepaula1@gmail.com>
2025-05-10 09:04:01 -0300

    feat(backend): completing our user routes

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { getRecommendedUsers, getMyFriends, sendFriendResquest, acceptFriendResquest } from "../controllers/user.controller.js"
+import { getRecommendedUsers, getMyFriends, sendFriendResquest, acceptFriendResquest, getFriendResquests, getOutgoingFriendReqs } from "../controllers/user.controller.js"
 import { protectRoute } from "../middleware/auth.middleware.js"
 
 const router = express.Router()
@@ -12,5 +12,8 @@ router.get("/friends", getMyFriends)
 
 router.post("/friend-request/:id", sendFriendResquest)
 router.put("/friend-request/:id/accept", acceptFriendResquest)
+
+router.get("/friend-requests", getFriendResquests)
+router.get("/outgoing-friend-requests", getOutgoingFriendReqs)
 
 export default router
