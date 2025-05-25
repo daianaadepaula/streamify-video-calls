f2eaf673f8074ce548520946b000cdeb7387a52e
daianaadepaula <daianaadepaula1@gmail.com>
2025-05-25 09:00:04 -0300

    feat(frontend): chat page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -43,7 +43,15 @@ const App = () => {
 						<Navigate to={!isAuthenticated ? "/login" : "/onboarding"} />
 					)} />
 				<Route path="/call" element={isAuthenticated ? <CallPage /> : <Navigate to="/login" />} />
-				<Route path="/chat" element={isAuthenticated ? <ChatPage /> : <Navigate to="/login" />} />
+				<Route path="/chat/:id" element={
+					isAuthenticated && isOnboarded ? (
+						<Layout showSidebar={false}>
+							<ChatPage />
+						</Layout>
+					) : (
+						<Navigate to={!isAuthenticated ? "/login" : "/onboarding"} />
+					)
+				} />
 				<Route path="/onboarding" element={isAuthenticated ? (
 					!isOnboarded ? (<OnboardingPage />) : (<Navigate to="/" />)
 				) : (
